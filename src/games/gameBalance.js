73acc5a0b8cdf219a0c32349477055a36cd023a6
Andrey Pryadko <andrey@pryadko.com>
2018-08-02 21:45:01 +0300

    work on brain-balance

--- src/games/gameBalance.js
+++ src/games/gameBalance.js
@@ -7,8 +7,33 @@ const minNumber = 100;
 const maxNumber = 10000;
 
 const getBalanceNumber = (number) => {
-
+  const stringNumber = String(number);
+  const lengthNumber = stringNumber.length;
 };
+
+const getBigNumber = (number, i) => {
+  const stringNumber = String(number);
+  let result = stringNumber[0];
+  if (i === stringNumber.length) {
+    return result;
+  }
+  result = stringNumber[i] > stringNumber[i + 1] ? stringNumber[i] : stringNumber[i + 1];
+  return getBigNumber(number, i + 1);
+};
+
+const getSmallNumber = (number, i) => {
+  const stringNumber = String(number);
+  let result = stringNumber[0];
+  if (i === stringNumber.length) {
+    return result;
+  }
+  result = stringNumber[i] < stringNumber[i + 1] ? stringNumber[i] : stringNumber[i + 1];
+  return getSmallNumber(number, i + 1);
+};
+
+
+// есть мысль разбить задачу на два этапа, 1.большая цифра отдает меньшей.
+// 2.расставляем цифры в нужном порядке
 
 const gameBalance = () => {
   const question = generateRandomNumber(minNumber, maxNumber);
