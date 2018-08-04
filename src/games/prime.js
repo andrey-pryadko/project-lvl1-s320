1b7aa62aa339a00c6493ba90408b530a7b74367a
Andrey Pryadko <andrey@pryadko.com>
2018-08-04 16:58:35 +0300

    fix brain prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,6 +3,9 @@ import generateRandomNumber from '../utils';
 import startGame from '..';
 
 const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
